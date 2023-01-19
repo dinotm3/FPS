798d752af9fffc6dcf3f7fc3c5a55ec2195e99d4
Dino Tretinjak Mesarić <dtretin@racunarstvo.hr>
2023-01-19 07:11:18 +0100

    added bullets and bullet holes. TODO: add pooling for bullets

--- Assets/Input/PlayerController.cs
+++ Assets/Input/PlayerController.cs
@@ -6,26 +6,63 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
-    private PlayerInput playerInput;
     [SerializeField]
-    private float playerSpeed = 20f;
+    private float playerSpeed = 5f;
     [SerializeField]
     private float jumpHeight = 9.0f;
     [SerializeField]
     private float gravityValue = -9.81f;
+    [SerializeField]
+    private float rotationSpeed = 5f;
+    [SerializeField]
+    private GameObject bulletPrefab;
+    [SerializeField]
+    private Transform bulletParent;
+    [SerializeField]
+    private Transform gunTransform;
 
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private PlayerInput playerInput;
     private InputAction moveAction;
     private InputAction jumpAction;
+    private InputAction shootAction;
+    private Transform cameraTransform;
+    [SerializeField]
+    private float bulletHitMissDistance = 25f;
 
-    private void Start()
+    private void Awake()
     {
         controller = GetComponent<CharacterController>();
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions["Move"];
         jumpAction = playerInput.actions["Jump"];
+        cameraTransform = Camera.main.transform;
+        shootAction = playerInput.actions["Shoot"];
+    }
+
+    private void OnEnable()
+    {
+        shootAction.performed += _ => ShootGun();
+    }
+
+    private void ShootGun()
+    {
+        RaycastHit hit;
+        GameObject bullet = GameObject.Instantiate(bulletPrefab, gunTransform.position + new Vector3(0, 0, 1), Quaternion.identity, bulletParent);
+        BulletController bulletController = bullet.GetComponent<BulletController>();
+        if (Physics.Raycast(cameraTransform.position, cameraTransform.transform.forward, out hit, Mathf.Infinity))
+        {
+            bulletController.target = hit.point;
+            bulletController.hit = true;
+        } 
+        else 
+        {
+            bulletController.target = cameraTransform.position + cameraTransform.forward * bulletHitMissDistance;
+            bulletController.hit = false;
+        }
+
     }
 
     void Update()
@@ -38,6 +75,9 @@ public class PlayerController : MonoBehaviour
 
         Vector2 input = moveAction.ReadValue<Vector2>();
         Vector3 move = new Vector3(input.x, 0, input.y);
+        // Camera direction when moving
+        move = move.x * cameraTransform.right.normalized + move.z * cameraTransform.forward.normalized;
+        move.y = 0f;
         controller.Move(move * Time.deltaTime * playerSpeed);
 
 
@@ -49,5 +89,10 @@ public class PlayerController : MonoBehaviour
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+
+        // Rotate towards camera
+        float targetAngle = cameraTransform.eulerAngles.y;
+        Quaternion targetRotation = Quaternion.Euler(0, targetAngle, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bulletHole;
+
+    private float speed = 50f;
+    private float timeToDestroy = 3f;
+
+    public Vector3 target { get; set; }
+    public bool hit { get; set; }
+
+    private void OnEnable()
+    {
+        Destroy(gameObject, timeToDestroy);
+    }
+
+    // Switch with pooling
+    void Update()
+    {
+        // add layermask as last argument in MoveTowards to stop bullets from colliding with everything
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        // In case we miss (shoot in air)
+        if (!hit && Vector3.Distance(transform.position, target) < .01f) {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit: " + collision.gameObject.name);
+        ContactPoint contact = collision.GetContact(0);
+        GameObject.Instantiate(bulletHole, contact.point + contact.normal * .0001f, Quaternion.LookRotation(contact.normal));
+        Destroy(gameObject);
+    }
+}
