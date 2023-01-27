241acf4fdef726ea8ebd65d36c2ac8379fbf494a
Dino Tretinjak Mesarić <dtretin@racunarstvo.hr>
2023-01-27 05:21:00 +0100

    fixed health bar. tried adding text but cannot access text from script

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerManager : MonoBehaviour
 {
     public int healthpoints;
     public Slider healthBar;
-    
+    private TMP_Text healthBarText;
+    public int maxHealth;
+
     private void Awake()
     {
         healthpoints = 100;
+        maxHealth = 150;
+        healthBarText = GetComponent<TMP_Text>();
+        Debug.Log("healthBarText: " + healthBarText);
     }
 
     public bool TakeHit()
@@ -30,7 +36,7 @@ public class PlayerManager : MonoBehaviour
         if (healthBar != null)
         {
             healthBar.value = healthpoints;
-
+            healthBarText.text = healthpoints.ToString() + " / " + maxHealth.ToString();
         }
     }
 }
